8e61f3b89c9a9d3c2de56af0340e5b29d25be5f3
gustahh <gustavocarlos206@gmail.com>
2024-05-07 23:42:41 -0300

    novas tabelas

--- tabelas/veterinaria_animal.sql
+++ tabelas/veterinaria_animal.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: veterinaria
 -- ------------------------------------------------------
--- Server version	8.0.35
+-- Server version	8.0.36
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -31,10 +31,11 @@ CREATE TABLE `animal` (
   `dtnasc` date DEFAULT NULL,
   `peso` float(10,2) DEFAULT NULL,
   `codcli` int DEFAULT NULL,
+  `foto` varchar(255) DEFAULT 'animal.jpg',
   PRIMARY KEY (`codanim`),
   KEY `codcli` (`codcli`),
   CONSTRAINT `animal_ibfk_1` FOREIGN KEY (`codcli`) REFERENCES `cliente` (`CODCLI`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,7 +44,7 @@ CREATE TABLE `animal` (
 
 LOCK TABLES `animal` WRITE;
 /*!40000 ALTER TABLE `animal` DISABLE KEYS */;
-INSERT INTO `animal` VALUES (1,'Cachorro','Pitbull','Princesa','Grande','2021-05-21',24.00,3),(2,'Gato','Pelo Curto Brasileiro','Mika','Pequeno','2021-02-12',5.00,1),(3,'Gato','Pelo Curto Brasileiro','Cretino','Pequeno','2024-02-16',2.00,1);
+INSERT INTO `animal` VALUES (1,'Cachorro','Pitbull','Princesa','Grande','2021-05-21',24.00,3,'animal.jpg'),(2,'Gato','Pelo Curto Brasileiro','Mika','Pequeno','2021-02-12',5.00,1,'animal.jpg'),(3,'Gato','Pelo Curto Brasileiro','Cretino','Pequeno','2024-02-16',2.00,1,'animal.jpg'),(21,'Gato','Pelo curto brasileiro','Garfield','Grande','2021-09-28',8.00,1,'1715135416_apps.46534.13715883802827517.eee7f8a7-1e95-4335-860b-982691cd78b0.png');
 /*!40000 ALTER TABLE `animal` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -56,4 +57,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-04-24 11:04:28
+-- Dump completed on 2024-05-07 23:41:50
--- tabelas/veterinaria_funcionario.sql
+++ tabelas/veterinaria_funcionario.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: veterinaria
 -- ------------------------------------------------------
--- Server version	8.0.35
+-- Server version	8.0.36
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -55,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-04-24 11:04:28
+-- Dump completed on 2024-05-07 23:41:50
